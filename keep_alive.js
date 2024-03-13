7080a4d4ae4753e558f44a1e2c2ad7b232a3c947
Jet Gumisad Vergara <59007045+Kurumi0343@users.noreply.github.com>
2024-03-13 19:41:08 +0800

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,1 +1,1 @@
-require("http").createServer((_, res) => res.end("Hello World!")).listen(3000, () => console.log("Yay the web server is online!"));
+require("http").createServer((_, res) => res.end("Hello World!")).listen(8080, () => console.log("Yay the web server is online!"));
