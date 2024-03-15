d6ece51f77e6b0fa52bacbf24b1ce4b93deb71cc
Jet Gumisad Vergara <59007045+Kurumi0343@users.noreply.github.com>
2024-03-15 21:36:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -159,6 +159,7 @@ client.on('interactionCreate', async (interaction) => {
           const currentTimestamp = Math.floor(Date.now() / 1000);
           const daysInServer = Math.floor(currentTimestamp - joinTimestamp);
           const endDate = Math.floor(currentTimestamp + parseTimeToSeconds(config.role_duration) - daysInServer);
+          console.log(endDate)
           if (joinTimestamp > sixtyDaysAgo) {
             if (newbieRole) {
               member.roles.add(newbieRole)
