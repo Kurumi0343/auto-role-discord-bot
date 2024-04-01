ae4093190fc4f01b8c7b0a3c54e762b51dbf01f5
Jet Gumisad Vergara <59007045+Kurumi0343@users.noreply.github.com>
2024-04-01 16:35:14 +0800

    Add files via upload

--- commands.js
+++ commands.js
@@ -1,42 +1,35 @@
 // commands.js
 
-module.exports = 
-[
-    {
-      name: 'ping',
-      description: 'pong',
-      options: [
-      ],
-    },
-    {
-      name: 'validate',
-      description: 'reset role of all new users for the past 59 days',
-      options: [
-        {
-          name: 'duration',
-          type: 3,
-          description: 'role duration',
-          required: false,
-        }
-      ],
-    },
-    {
-      name: 'setrole',
-      description: 'set role',
-      options: [
-        {
-          name: 'user_id',
-          type: 3,
-          description: 'user to set',
-          required: true,
-        },
-        {
-          name: 'duration',
-          type: 3,
-          description: 'role duration',
-          required: true,
-        }
-      ],
-    },
+module.exports = [{
+    name: 'ping',
+    description: 'pong',
+    options: [],
+  },
+  {
+    name: 'validate',
+    description: 'reset role of all new users for the past 59 days',
+    options: [{
+      name: 'duration',
+      type: 3,
+      description: 'role duration',
+      required: false,
+    }],
+  },
+  {
+    name: 'setrole',
+    description: 'set role',
+    options: [{
+        name: 'user_id',
+        type: 3,
+        description: 'user to set',
+        required: true,
+      },
+      {
+        name: 'duration',
+        type: 3,
+        description: 'role duration',
+        required: true,
+      }
+    ],
+  },
 ];
-  
