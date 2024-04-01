7fdb99181e1884b0396d05b23689a30f5f42a9fd
Jet Gumisad Vergara <59007045+Kurumi0343@users.noreply.github.com>
2024-04-01 16:34:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const keep_alive = require('./keep_alive.js')
-const firebaseadmin = require('firebase-admin');
+const mongoose = require('mongoose');
+
+const TOKEN = ""
+const CLIENT_ID = ""
+const mongoURL = ""
+const ROLE_ID = "1224237860564238346"
+const ROLE_DURATION = "2k"
 
 const {
   Client,
@@ -8,7 +14,8 @@ const {
   InteractionType,
   EmbedBuilder,
   ActionRowBuilder,
-  ButtonBuilder
+  ButtonBuilder,
+  PermissionsBitField
 } = require('discord.js');
 const {
   REST
@@ -16,23 +23,20 @@ const {
 const {
   Routes
 } = require('discord-api-types/v9');
-const config = require('./config.json');
 const commands = require('./commands.js');
-const {
-  error
-} = require('console');
+
+const autoRoleSchema = new mongoose.Schema({
+  serverId: String,
+  endDate: String,
+  joinDate: String,
+  roleId: String,
+  userId: String
+});
+
+const autoRole = mongoose.model('autoRole', autoRoleSchema)
+
 const countdownTimers = {};
 
-firebaseadmin.initializeApp({
-  credential: firebaseadmin.credential.cert({
-    projectId: process.env.PROJECT_ID,
-    clientEmail: process.env.CLIENT_EMAIL,
-    privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n')
-  }),
-  databaseURL: 'https://ucomebooster-18265-default-rtdb.firebaseio.com' // Replace 'your-project-id' with your Firebase project ID
-});
-const db = firebaseadmin.database();
-const ref = db.ref('users');
 const client = new Client({
   intents: [
     IntentsBitField.Flags.Guilds,
@@ -68,65 +72,69 @@ function parseTimeToSeconds(timeString) {
   }
 }
 
-async function updateCountdown(entry) {
+async function updateCountdown(userData) {
   try {
-    const userRef = ref.child(entry);
-    userRef.once('value')
-      .then(async snapshot => {
-        const data = snapshot.val();
-        const interact = client.guilds.cache.get(config.server_id);
-        const currentDate = Math.floor(Date.now() / 1000);
-        const remainingTime = data.endDate - currentDate;
-        if (remainingTime <= 0) {
-          if (data.isNew) {
-            const roleToRemove = interact.roles.cache.get(data.roleid);
-            if (data) {
-              const member = await interact.members.fetch(data.userid);
-              member.roles.remove(roleToRemove).then(() => {
-                  userRef.remove()
-                })
-                .catch((error) => console.log(error));
-            }
-            clearInterval(countdownTimers[data.userid]);
-            delete countdownTimers[data.userid];
-          }
-        }
-      })
-      .catch(error => {
-        console.error('Error retrieving data:', error);
-      });
+    const interact = client.guilds.cache.get(userData.serverId);
+    const currentDate = Math.floor(Date.now() / 1000);
+    const remainingTime = userData.endDate - currentDate;
+    if (remainingTime <= 0) {
+      const roleToRemove = interact.roles.cache.get(userData.roleId);
+      const fetchedMember = await interact.members.fetch(userData.userId);
+      fetchedMember.roles.remove(roleToRemove).then(() => {
+          autoRole.findOneAndDelete({
+            userId: userData.userId
+          }).then(itemDelete => {
+            clearInterval(countdownTimers[itemDelete.userId]);
+            delete countdownTimers[itemDelete.userId];
+          }).catch(_ => null)
+        })
+        .catch((error) => console.log(error));
+    }
   } catch (error) {
-    console.log(error + entry)
+    console.log(error, userData)
   }
 }
 
-client.on('ready', async () => {
-  ref.once('value')
-    .then(snapshot => {
-      const childNames = Object.keys(snapshot.val() || {});
-      childNames.forEach(childName => {
-        countdownTimers[childName] = setInterval(() => {
-          updateCountdown(childName);
-        }, 1000);
-      });
-    })
-    .catch(error => {
+process.on('unhandledRejection', error => {
+  console.error('Unhandled promise rejection:', error);
+});
 
-    });
+client.on('ready', async () => {
+  await mongoose.connect(process.env.mongoURL || mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  autoRole.find({}, 'userId')
+    .then(rolesData => {
+      if (rolesData.length > 0) {
+        rolesData.forEach(data => {
+          countdownTimers[data.userId] = setInterval(() => {
+            updateCountdown(data);
+          }, 1000);
+        })
+      }
+    })
+
   const commandsArray = commands.map((command) => ({
     ...command,
     type: 1,
   }));
+
   const rest = new REST({
     version: '9'
-  }).setToken(process.env.TOKEN);
+  }).setToken(process.env.TOKEN || TOKEN);
   try {
     console.log('Started refreshing application (/) commands.');
-    await rest.put(
-      Routes.applicationGuildCommands(process.env.CLIENT_ID, config.server_id), {
-        body: commandsArray
-      },
-    );
+    client.guilds.cache.forEach(async guild => {
+      await rest.put(
+        Routes.applicationGuildCommands(process.env.CLIENT_ID || CLIENT_ID, guild.id), {
+          body: commandsArray
+        },
+      );
+    });
+    await Promise.all(client.guilds.cache.map(guild => guild.members.fetch()));
+    await Promise.all(client.guilds.cache.map(guild => guild.channels.fetch()));
+    await Promise.all(client.guilds.cache.map(guild => guild.roles.fetch()));
     console.log('Successfully reloaded application (/) commands.');
   } catch (error) {
     console.error(error);
@@ -134,7 +142,13 @@ client.on('ready', async () => {
 });
 
 client.on('interactionCreate', async (interaction) => {
-
+  if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageGuild)) {
+    await interaction.reply({
+      content: "Only Staff can use this command",
+      ephemeral: true
+    })
+    return
+  }
   if (interaction.isCommand()) {
     const {
       commandName,
@@ -143,51 +157,57 @@ client.on('interactionCreate', async (interaction) => {
     } = interaction;
     if (commandName === 'ping') {
       try {
-        await interaction.reply({
-          content: `This is bot is online`,
-          ephemeral: true
-        });
+        if (!interaction.deferred && !interaction.replied) {
+          await interaction.reply({
+            content: `This is bot is online`,
+            ephemeral: true
+          });
+        }
       } catch (error) {
         console.log(error)
       }
     }
     if (commandName === 'validate') {
-      await interaction.guild.members.fetch();
+      if (options.getString('duration') && parseTimeToSeconds(options.getString('duration')) <= 0) {
+        await interaction.reply({
+          content: `${(parseTimeToSeconds(options.getString('duration')) === '-10') ? 'Sytanx Error, correct format: 1s, 1m, 1h, 1d, 1k' : 'Not supported time date'}`,
+          ephemeral: true
+        });
+        return
+      }
       interaction.guild.members.cache.forEach(async member => {
         try {
-          const newbieRole = interaction.guild.roles.cache.get(config.role_id);
+          const newbieRole = interaction.guild.roles.cache.get(ROLE_ID);
           const joinTimestamp = Math.floor(member.joinedTimestamp / 1000);
           const sixtyDaysAgo = Math.floor(new Date().getTime() / 1000) - (60 * 24 * 60 * 60);
           const currentTimestamp = Math.floor(Date.now() / 1000);
           const daysInServer = Math.floor(currentTimestamp - joinTimestamp);
-          const endDate = Math.floor(currentTimestamp + parseTimeToSeconds(config.role_duration) - daysInServer);
+          const endDate = Math.floor(currentTimestamp + parseTimeToSeconds(ROLE_DURATION) - daysInServer);
           if (joinTimestamp > sixtyDaysAgo) {
             if (newbieRole) {
-              member.roles.add(newbieRole)
+              await member.roles.add(newbieRole)
                 .then(() => {
-                  const dataUser = member.id;
-                  const data = ref.child(dataUser);
-                  data.once('value')
-                    .then(snapshot => {
-                      const data = snapshot.val();
-                      ref.child(dataUser).set({
-                          userid: dataUser,
-                          isNew: true,
-                          joinDate: currentTimestamp,
-                          endDate: endDate,
-                          roleid: config.role_id,
-                        })
-                        .then(() => {})
-                        .catch(error => {
-                          console.error('Error pushing data:', error);
-                        });
-                      countdownTimers[dataUser] = setInterval(() => {
-                        updateCountdown(dataUser);
-                      }, 1000);
-                    })
-                    .catch(error => {
-                      console.error('Error retrieving data:', error);
-                    });
+                  autoRole.findOne({
+                    userId: member.id
+                  }).then(async userData => {
+                    if (!userData) {
+                      const autoRoleData = new autoRole({
+                        serverId: interaction.guild.id,
+                        endDate: Math.floor(currentTimestamp + parseTimeToSeconds(options.getString('duration'))) || endDate,
+                        joinDate: currentTimestamp,
+                        roleId: ROLE_ID,
+                        userId: member.id
+                      })
+                      autoRoleData.save().then(_ => {
+                        countdownTimers[autoRoleData.userId] = setInterval(() => {
+                          updateCountdown(autoRoleData);
+                        }, 1000);
+                      })
+                    } else {
+                      userData.endDate = endDate;
+                      await userData.save()
+                    }
+                  })
                 })
                 .catch((error) => console.error('Error adding role on join:', error));
             }
@@ -196,18 +216,19 @@ client.on('interactionCreate', async (interaction) => {
           console.log(error)
         }
       });
+
       try {
         await interaction.reply({
           content: 'Validated!',
           ephemeral: true
         });
-      } catch(error) {
-        
+      } catch {
+
       }
     }
     if (commandName === 'setrole') {
       try {
-        const newbieRole = interaction.guild.roles.cache.get(options.getString('role_id'));
+        const newbieRole = interaction.guild.roles.cache.get(ROLE_ID);
         const fetchUser = await interaction.guild.members.fetch(options.getString('user_id'));
         const currentDate = Math.floor(Date.now() / 1000);
         if (parseTimeToSeconds(options.getString('duration')) <= 0) {
@@ -219,52 +240,53 @@ client.on('interactionCreate', async (interaction) => {
         }
         const endDate = currentDate + parseTimeToSeconds(options.getString('duration'));
         if (newbieRole) {
-          const optionsUser = options.getString('user_id');
-          fetchUser.roles.add(newbieRole)
-            .then(() => {
-              interaction.reply({
-                content: 'Role added!',
-                ephemeral: true
-              });
-              const data = ref.child(optionsUser);
-              data.once('value')
-                .then(snapshot => {
-                  const data = snapshot.val();
-                  ref.child(optionsUser).set({
-                      userid: optionsUser,
-                      isNew: true,
-                      joinDate: currentDate,
-                      endDate: endDate,
-                      roleid: options.getString('role_id'),
-                    })
-                    .then(() => {})
-                    .catch(error => {
-                      console.error('Error pushing data:', error);
-                    });
-                  countdownTimers[optionsUser] = setInterval(() => {
-                    updateCountdown(optionsUser);
-                  }, 1000);
-                })
-                .catch(error => {
-                  console.error('Error retrieving data:', error);
+          await fetchUser.roles.add(newbieRole)
+            .then(async () => {
+              if (!interaction.deferred && !interaction.replied) {
+                await interaction.reply({
+                  content: 'Role added!',
+                  ephemeral: true
                 });
+              }
 
+              autoRole.findOne({
+                userId: fetchUser.id
+              }).then(userData => {
+                if (!userData) {
+                  const autoRoleData = new autoRole({
+                    serverId: interaction.guild.id,
+                    endDate: endDate,
+                    joinDate: currentDate,
+                    roleId: ROLE_ID,
+                    userId: fetchUser.id
+                  })
+                  autoRoleData.save().then(_ => {
+                    countdownTimers[autoRoleData.userId] = setInterval(() => {
+                      updateCountdown(autoRoleData);
+                    }, 1000);
+                  })
+                } else {
+                  userData.roleId = ROLE_ID;
+                  userData.endDate = endDate;
+                  userData.save().then(_ => {
+                    clearInterval(countdownTimers[userData.userId]);
+                    delete countdownTimers[userData.userId];
+
+                    countdownTimers[userData.userId] = setInterval(() => {
+                      updateCountdown(userData);
+                    }, 1000);
+                  })
+                }
+              })
             })
             .catch((error) => {
-              interaction.reply({
-                content: 'Role not added!',
-                ephemeral: true
-              })
               console.log(error)
             });
         } else {
           console.error(`Role with ID ${optionsUser} not found.`);
         }
       } catch (error) {
-        interaction.reply({
-          content: error,
-          ephemeral: true
-        })
+        console.log(error)
       }
     }
   }
@@ -273,35 +295,30 @@ client.on('interactionCreate', async (interaction) => {
 
 client.on('guildMemberAdd', async (member) => {
   try {
-    const newbieRole = member.guild.roles.cache.get(config.role_id);
+    const newbieRole = member.guild.roles.cache.get(ROLE_ID);
     const currentDate = Math.floor(Date.now() / 1000);
-    const endDate = currentDate + parseTimeToSeconds(config.role_duration);
+    const endDate = currentDate + parseTimeToSeconds(ROLE_DURATION);
     if (newbieRole) {
-      member.roles.add(newbieRole)
+      await member.roles.add(newbieRole)
         .then(() => {
-          const dataUser = member.id;
-          const data = ref.child(dataUser);
-          data.once('value')
-            .then(snapshot => {
-              const data = snapshot.val();
-              ref.child(dataUser).set({
-                  userid: dataUser,
-                  isNew: true,
-                  joinDate: currentDate,
-                  endDate: endDate,
-                  roleid: config.role_id,
-                })
-                .then(() => {})
-                .catch(error => {
-                  console.error('Error pushing data:', error);
-                });
-              countdownTimers[dataUser] = setInterval(() => {
-                updateCountdown(dataUser);
-              }, 1000);
-            })
-            .catch(error => {
-              console.error('Error retrieving data:', error);
-            });
+          autoRole.findOne({
+            userId: member.id
+          }).then(userData => {
+            if (!userData) {
+              const autoRoleData = new autoRole({
+                serverId: member.guild.id,
+                endDate: endDate,
+                joinDate: currentDate,
+                roleId: ROLE_ID,
+                userId: member.id
+              })
+              autoRoleData.save().then(_ => {
+                countdownTimers[autoRoleData.userId] = setInterval(() => {
+                  updateCountdown(autoRoleData);
+                }, 1000);
+              })
+            }
+          })
         })
         .catch((error) => console.error('Error adding role on join:', error));
     } else {
@@ -312,4 +329,4 @@ client.on('guildMemberAdd', async (member) => {
   }
 });
 
-client.login(process.env.TOKEN);
+client.login(process.env.TOKEN || TOKEN);
